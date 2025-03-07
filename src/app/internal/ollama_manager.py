63813da87a4414a74cc6e37d082731649d5d3d58
tzway <zhengyang.c@outlook.com>
2025-03-07 18:34:25 -0500

    add streamed chat service

---
+++ src/app/internal/ollama_manager.py
@@ -0,0 +1,29 @@
+from fastapi.responses import StreamingResponse
+from .config_manager import load_config
+from ollama import AsyncClient
+import json
+
+config = load_config()
+OLLAMA_ENDPOINT, DEFAULT_MODEL = config["ollama_endpoint"], config["default_model"]
+client = AsyncClient(
+    host=OLLAMA_ENDPOINT,
+)
+
+async def generate_chat_stream(message: str, context: list):
+    """Chat with the Ollama model and return the response as a Server-Sent Event."""
+    context.append({'role': 'user', 'content': message})
+
+    stream = client.chat(
+        model=DEFAULT_MODEL,
+        messages=context,
+        stream=True,
+    )
+    async for chunk in await stream:
+        cleaned_data = {
+            "done": chunk.done,
+            "content": chunk.message.content,
+        }
+        yield f"data: {json.dumps(cleaned_data)}\n\n"
+        context.append(chunk.message)
+    return
+
