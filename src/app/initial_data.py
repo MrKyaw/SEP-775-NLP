1a081fe4bcd1d706d2e871703cbc0a289dd3011b
tzway <zhengyang.c@outlook.com>
2025-03-08 02:51:21 -0500

    add database initialization script

---
+++ src/app/initial_data.py
@@ -0,0 +1,25 @@
+# PYTHONPATH=SEP-775-NLP/src python3 app/initial_data.py
+
+import logging
+
+from sqlmodel import Session
+
+from app.db import engine, init_db
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def init() -> None:
+    with Session(engine) as session:
+        init_db(session)
+
+
+def main() -> None:
+    logger.info("Creating initial data")
+    init()
+    logger.info("Initial data created")
+
+
+if __name__ == "__main__":
+    main()
