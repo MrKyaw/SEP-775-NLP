606e49b160a899ceef6da5ebc413ad2696bb9904
tzway <zhengyang.c@outlook.com>
2025-03-08 16:55:10 -0500

    add login access token logic

--- src/app/dependencies.py
+++ src/app/dependencies.py
@@ -14,21 +14,28 @@ from sqlmodel import Session
 from app.models import TokenPayload, User
 from app.db import engine
 
-reusable_oauth2 = OAuth2PasswordBearer(
-    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
-)
+
 
 def get_db() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
-    
+
 SessionDep = Annotated[Session, Depends(get_db)]
-TokenDep = Annotated[str, Depends()]
+
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
+)
+
+TokenDep = Annotated[str, Depends(reusable_oauth2)]
 
 def get_current_user(session: SessionDep, token: TokenDep) -> User:
+    """
+    Get the current user from the JWT token
+    """
+    # if the token expires, the user cannot be authenticated
     try:
         payload = jwt.decode(
-            token, settings.SECRET_KEY, algorithms=["HS256"]
+            token, settings.SECRET_KEY, algorithms=[settings.JWT_ALGORITHM]
         )
         token_data = TokenPayload(**payload)
     except (InvalidTokenError, ValidationError):
@@ -36,6 +43,8 @@ def get_current_user(session: SessionDep, token: TokenDep) -> User:
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Could not validate credentials",
         )
+    # validate the token data
+    # token_data = {"exp": expire, "sub": str(subject)}
     user = session.get(User, token_data.sub)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
