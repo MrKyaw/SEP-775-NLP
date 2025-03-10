1c219a41977e73a58ad213311e7aac015de1bedf
tzway <zhengyang.c@outlook.com>
2025-03-09 20:31:30 -0400

    add logger

--- src/app/main.py
+++ src/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import chat, siteconf, user
 from app.settings import settings
+from app.logger import logger
 
 app = FastAPI()
 
@@ -20,3 +21,5 @@ api_router.include_router(siteconf.router)
 api_router.include_router(chat.router)
 api_router.include_router(user.router)
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
+logger.info("App initialized")
