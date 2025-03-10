a8d21f7a1ed37eaaa74bc71e32d377ea88a0d5eb
tzway <zhengyang.c@outlook.com>
2025-03-10 11:27:14 -0400

    synchronous chat

--- src/app/routers/chat.py
+++ src/app/routers/chat.py
@@ -8,14 +8,18 @@ from sqlmodel import select, func
 from app import db
 import uuid
 from app.logger import logger
-
+from sqlmodel import Session
+from app.db import engine
 
 from app.services.siteconf_manager import load_config
-from ollama import AsyncClient
+from ollama import AsyncClient, Client
 
 config = load_config()
 OLLAMA_ENDPOINT, DEFAULT_MODEL = config["ollama_endpoint"], config["default_model"]
-client = AsyncClient(
+async_client = AsyncClient(
+    host=OLLAMA_ENDPOINT,
+)
+client = Client(
     host=OLLAMA_ENDPOINT,
 )
 
@@ -81,31 +85,88 @@ async def send_message(
     chat = session.exec(select(Chat).where(Chat.id == chat_id)).first()
     if not chat or chat.owner != current_user:
         raise HTTPException(status_code=403, detail=f"You do not have permission to access chat-{chat_id}")
-
-    # append user message to context
-    if chat.title is None:
-        chat.title = request.message[:150]
     chat.context.append({'role': 'user', 'content': request.message})
+    session.commit()
 
     async def generate_chat_wrapper():
         """Chat with the Ollama model and return the response as a Server-Sent Event."""
-        stream = client.chat(
-            model=DEFAULT_MODEL,
-            messages=chat.context,
-            stream=True,
-        )
+        with Session(engine) as inner_session:
+            chat = inner_session.exec(select(Chat).where(Chat.id == chat_id)).first()
 
-        full_response = ""
-        async for chunk in await stream:
-            full_response += chunk.message.content
-            yield chunk.message.content
-        chat.context.append({"role":"assistant", "content":full_response})
-        session.add(chat)
-        session.commit()
-        session.refresh(chat)
-        logger.debug(chat.context)
+            stream = async_client.chat(
+                model=DEFAULT_MODEL,
+                messages=chat.context,
+                stream=True,
+            )
 
-        return
+            full_response = ""
+            async for chunk in await stream:
+                full_response += chunk.message.content
+                yield chunk.message.content
+            if chat.title is None:
+                chat.title = full_response[:150]
+            chat.context.append({"role":"assistant", "content":full_response})
+            logger.debug(chat.context)
+            # session.add(chat)
+            inner_session.commit()
+            # session.refresh(chat)
+            logger.debug(chat.context)
+
+
+
+    return StreamingResponse(
+        generate_chat_wrapper(),
+        media_type="text/event-stream",
+        headers={
+            "Cache-Control": "no-cache",
+            "X-Accel-Buffering": "no",
+        }
+    )
+
+@router.post("/{chat_id}/sync")
+def send_message(
+    session: SessionDep,
+    current_user: CurrentUser,
+
+    chat_id: uuid.UUID,
+    request: MessageRequest = Body(..., example={"message": "Hello"}),
+):
+    """Chat with the Ollama model and return the response as a Server-Sent Event."""
+    # load chat from db
+    # chat = db.get_chat_by_id(session=session, chat_id=chat_id)
+    chat = session.exec(select(Chat).where(Chat.id == chat_id)).first()
+    if not chat or chat.owner != current_user:
+        raise HTTPException(status_code=403, detail=f"You do not have permission to access chat-{chat_id}")
+    
+    if not chat.context or chat.context[-1]['role'] != 'user':
+        chat.context = chat.context + [{'role': 'user', 'content': request.message}]
+    else:
+        chat.context[-1] = [{'role': 'user', 'content': request.message}]
+        chat.context = chat.context[:]
+    session.commit()
+
+    def generate_chat_wrapper():
+        """Chat with the Ollama model and return the response as a Server-Sent Event."""
+        with Session(engine) as inner_session:
+            chat = inner_session.exec(select(Chat).where(Chat.id == chat_id)).first()
+
+            stream = client.chat(
+                model=DEFAULT_MODEL,
+                messages=chat.context,
+                stream=True,
+            )
+
+            full_response = ""
+            for chunk in stream:
+                full_response += chunk.message.content
+                yield chunk.message.content
+            if chat.title is None:
+                chat.title = full_response[:150]
+
+            chat.context = chat.context + [{"role":"assistant", "content":full_response}]
+            logger.debug(chat.context)
+            inner_session.commit()
+            logger.debug(chat.context)
 
 
 
